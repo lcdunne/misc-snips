f37e92023241d4c85c68e8a99f97af2840229938
l <56237933+lcdunne@users.noreply.github.com>
2023-12-24 21:24:12 +0000

    Cleanup (comments, unused code)

--- flask/request-validation/decoration.py
+++ flask/request-validation/decoration.py
@@ -10,18 +10,14 @@ def validate_function_arg(arg_name, arg_value):
     return TypeAdapter(arg_name).validate_python(arg_value)
 
 
-def validate_path_parameters(
+def validate_path_params(
     path_params: dict, path_model: BaseModel, annotations: dict
 ) -> dict:
     # For custom path type converters: https://stackoverflow.com/a/32237936/7728410
     if path_params and path_model is None:
-        # if not annotations:
-        #     # Developer has used @validate but no model and no type hints
-        #     # TODO: Might not need to handle this
-        #     abort(
-        #         500,
-        #         "Must either provide a path_model or use type hints in view function if using url parameters",
-        #     )
+        # Path parameters provided but no model given - use type hints
+        if not annotations:
+            raise TypeError(f"Untyped path parameters: {list(path_params.keys())}")
 
         validated_args = {}
         for arg, value in path_params.items():
@@ -29,34 +25,11 @@ def validate_path_parameters(
                 validated_args[arg] = validate_function_arg(annotations[arg], value)
             except ValidationError as e:
                 abort(400, e)
-            except KeyError as e:
-                raise KeyError(f"{e}. Check the flask route function type hints")
         return validated_args
-    # elif not path_params and path_model is not None:
-    #     # TODO: Might not need to handle this
-    #     abort(500, "Path model provided but no url parameters in path")
     return as_model(path_params, path_model)
 
 
-def validate_path_parameters_from_type_hints(
-    path_params: dict, annotations: dict
-) -> dict:
-    # For custom path type converters: https://stackoverflow.com/a/32237936/7728410
-    if not annotations:
-        # Because we want an error only if no type annotations and no path_model
-        raise TypeError(f"Untyped path parameters: {list(path_params.keys())}")
-
-    valid_view_args = {}
-    for arg, value in path_params.items():
-        try:
-            result = TypeAdapter(annotations[arg]).validate_python(value)
-        except ValidationError as e:
-            abort(400, e)
-        valid_view_args[arg] = result
-    return valid_view_args
-
-
-def validate_query_parameters(query_params, query_model):
+def validate_query_params(query_params, query_model):
     # Query params sent but query is None: End user might have just sent query params -> DO NOTHING
     # Query params not sent, but query not None: End user just didn't send query params -> DO NOTHING
     return as_model(query_params, query_model)
@@ -95,24 +68,15 @@ def validate(body: BaseModel = None, query: BaseModel = None, path: BaseModel = 
 
             # Validate the path parameters
             spec = getfullargspec(f)  # Get args & type hints from the route function
-
-            # if request.view_args and path is None:
-            #     # Args were provided to the path, but no path model was specified
-            #     request.path_params = validate_path_parameters_from_type_hints(
-            #         request.view_args, spec.annotations
-            #     )
-            # else:
-            request.path_params = validate_path_parameters(
+            request.path_params = validate_path_params(
                 request.view_args, path, spec.annotations
             )
 
             # Validate the query parameters
-            request.query_params = validate_query_parameters(
-                request.args.to_dict(), query
-            )
+            request.query_params = validate_query_params(request.args.to_dict(), query)
 
             # Validate the request body
-            request.body = {}
+            request.body = {}  # Argument could be made that this should be None
             if has_body(request):
                 request.body = validate_body(request.get_json(), body)
             return f(*args, **kwargs)
